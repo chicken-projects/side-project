68adacf6bea7d30ff4d77d11ad43ff15568008fa
wild-chicken <louh13192@duck.com>
2024-12-19 15:58:58 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 14F)
+        Label1.ForeColor = SystemColors.ActiveCaptionText
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(152, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "chicken's project"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaptionText
+        Button1.ForeColor = SystemColors.ControlLight
+        Button1.Location = New Point(12, 415)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(157, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "download latest script"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlLightLight
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        ForeColor = SystemColors.ControlText
+        Name = "Form1"
+        Text = "chicken's project"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
